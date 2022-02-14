4c992f679e721b1c9dde1a0c2d7a16263dafce46
Marco Mazzocchi <marco.mazzocchi.pc@gmail.com>
2022-02-14 09:02:27 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,34 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './components/App'
+import routes from './routes'
+
+import '../css/app.css'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+const vm = app.mount('#app')
+
+/* 
+
+import 'vfonts/Inter.css'
+
+import App from './components/App'
+
+
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+app.use(router)
+const vm = app.mount('#app') */
---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+  <hr />
+  <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link>
+  <hr />
+  <router-view></router-view>
+</template>
